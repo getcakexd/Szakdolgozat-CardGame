1e4dc0e2143a1da56359ea9a346cf8ebd15a1006
getcakexd <cxc72413@gmail.com>
2025-04-16 14:46:16 +0200

    api call fix

--- frontend/src/environments/api-config.ts
+++ frontend/src/environments/api-config.ts
@@ -1,4 +1,5 @@
 export const BASE_URL = window.location.origin;
 
-export const BACKEND_API_URL = 'http://localhost:8080/api';
+
+export const BACKEND_API_URL = BASE_URL.includes('localhost') ? 'http://localhost:8080' : BASE_URL + '/api';
 export const PROXY_API_URL = BASE_URL + '/api';
