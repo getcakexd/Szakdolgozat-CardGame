35ec2cb802e263fc856a89b3efdd626ee41d430b
getcakexd <cxc72413@gmail.com>
2025-04-16 14:16:24 +0200

    import fix

--- frontend/src/environments/social-auth.config.ts
+++ frontend/src/environments/social-auth.config.ts
@@ -1,5 +1,6 @@
 import { type SocialAuthServiceConfig, GoogleLoginProvider } from "@abacritt/angularx-social-login"
-import {GOOGLE_CLIENT_ID_LOCAL} from './secrets';
+//FOR LOCAL DEV
+//import {GOOGLE_CLIENT_ID_LOCAL} from './secrets';
 
 declare global {
   interface Window {
@@ -9,7 +10,10 @@ declare global {
   }
 }
 
-const GOOGLE_CLIENT_ID = window.env?.GOOGLE_CLIENT_ID || GOOGLE_CLIENT_ID_LOCAL;
+// For production
+// Remove for DEV
+const GOOGLE_CLIENT_ID = window.env?.GOOGLE_CLIENT_ID ;
+
 
 export const socialAuthServiceConfig: SocialAuthServiceConfig = {
   autoLogin: false,
