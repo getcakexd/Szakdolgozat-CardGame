6742ba74ecf6033513baea7a28292191dc6d7bc4
getcakexd <cxc72413@gmail.com>
2025-04-17 22:03:15 +0200

    removing contact related

--- backend/src/main/java/hu/benkototh/cardgame/backend/rest/repository/IContactRequestRepository.java
+++
@@ -1,9 +0,0 @@
-package hu.benkototh.cardgame.backend.rest.repository;
-
-import hu.benkototh.cardgame.backend.rest.Data.ContactRequest;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface IContactRequestRepository extends JpaRepository<ContactRequest, Long> {
-}
